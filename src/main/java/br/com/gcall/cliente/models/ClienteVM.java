a7b4a8bf0b27ae897f6c2344fc92f1423010806d
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2022-09-18 11:06:54 -0300

    Atualização cliente controller e service

---
+++ src/main/java/br/com/gcall/cliente/models/ClienteVM.java
@@ -0,0 +1,13 @@
+package br.com.gcall.cliente.models;
+
+public class ClienteVM {
+    private long cpf;
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+}
--- src/main/java/br/com/gcall/cliente/services/ClienteService.java
+++ src/main/java/br/com/gcall/cliente/services/ClienteService.java
@@ -1,6 +1,7 @@
 package br.com.gcall.cliente.services;
 
 import br.com.gcall.cliente.entity.Cliente;
+import br.com.gcall.cliente.models.ClienteVM;
 import br.com.gcall.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,12 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public Cliente findCreateClient(long cpf){
+    public Cliente findCreateClient(ClienteVM clienteVM){
         Cliente cliente = null;
         try {
-            cliente = clienteRepository.findByCpf(cpf);
+            cliente = clienteRepository.findByCpf(clienteVM.getCpf());
             if(cliente == null) {
-                cliente = new Cliente(cpf);
+                cliente = new Cliente(clienteVM.getCpf());
                 clienteRepository.save(cliente);
             }
         } catch (Exception e) {
